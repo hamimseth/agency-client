d048edba8a0533af156deb279702aa862d5e17ef
hamimseth <hamim72400@gmail.com>
2020-10-19 00:00:39 +0600

    final submit

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCre2V6B_WV0N2qKqIeOez42ooOg-qnoBs",
+    authDomain: "creative-agency01.firebaseapp.com",
+    databaseURL: "https://creative-agency01.firebaseio.com",
+    projectId: "creative-agency01",
+    storageBucket: "creative-agency01.appspot.com",
+    messagingSenderId: "837859441277",
+    appId: "1:837859441277:web:ea1da5afca572a89f3cf14"
+  };
+  export default firebaseConfig;
